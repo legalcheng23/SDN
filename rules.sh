c9d690511343c42e9fc2a0affecaf65307258cdd
Legal <legalcheng23@gmail.com>
2024-10-15 23:47:31 +0800

    Update rules.sh

--- rules.sh
+++ rules.sh
@@ -1,191 +1,124 @@
 #!/bin/bash
 
 # ONOS 控制器的地址和認證
-ONOS_CONTROLLER_IP="172.17.0.5"
-ONOS_USER="onos"
-ONOS_PASS="rocks"
+CONTROLLER_IP="192.168.1.1"
+USER="onos"
+PASSWORD="rocks"
 
-# 設置流表規則
+# 添加從 h1 到 h2 的流表規則
+curl -X POST -H "Content-Type: application/json" -d '{
+  "priority": 60000,
+  "isPermanent": true,
+  "deviceId": "of:0000000000000001",
+  "treatment": {
+    "instructions": [
+      {
+        "type": "OUTPUT",
+        "port": "2"
+      }
+    ]
+  },
+  "selector": {
+    "criteria": [
+      {
+        "type": "ETH_TYPE",
+        "ethType": "0x0800"
+      },
+      {
+        "type": "IP_PROTO",
+        "protocol": 1
+      },
+      {
+        "type": "IPV4_SRC",
+        "ip": "10.0.0.1/32"
+      },
+      {
+        "type": "IPV4_DST",
+        "ip": "10.0.0.2/32"
+      }
+    ]
+  }
+}' http://$CONTROLLER_IP:8181/onos/v1/flows --user $USER:$PASSWORD
 
-# 1. 允許來自 h1 的流量轉發到 h2
-curl --user $ONOS_USER:$ONOS_PASS -X POST http://$ONOS_CONTROLLER_IP:8181/onos/v1/flows --header "Content-Type:application/json" --data '{
-  "flows": [{
-    "priority": 40000,
-    "timeout": 0,
-    "isPermanent": true,
-    "deviceId": "of:0000000000000003",
-    "treatment": {
-      "instructions": [
-        {
-          "type": "OUTPUT",
-          "port": "2"
-        }
-      ]
-    },
-    "selector": {
-      "criteria": [
-        {
-          "type": "ETH_TYPE",
-          "ethType": "0x0800"
-        },
-        {
-          "type": "IPV4_SRC",
-          "ip": "10.0.0.1/32"
-        },
-        {
-          "type": "IPV4_DST",
-          "ip": "10.0.0.2/32"
-        }
-      ]
-    }
-  }]
-}'
+# 添加從 h2 到 h1 的流表規則
+curl -X POST -H "Content-Type: application/json" -d '{
+  "priority": 60000,
+  "isPermanent": true,
+  "deviceId": "of:0000000000000002",
+  "treatment": {
+    "instructions": [
+      {
+        "type": "OUTPUT",
+        "port": "1"
+      }
+    ]
+  },
+  "selector": {
+    "criteria": [
+      {
+        "type": "ETH_TYPE",
+        "ethType": "0x0800"
+      },
+      {
+        "type": "IP_PROTO",
+        "protocol": 1
+      },
+      {
+        "type": "IPV4_SRC",
+        "ip": "10.0.0.2/32"
+      },
+      {
+        "type": "IPV4_DST",
+        "ip": "10.0.0.1/32"
+      }
+    ]
+  }
+}' http://$CONTROLLER_IP:8181/onos/v1/flows --user $USER:$PASSWORD
 
-# 2. 允許來自 h2 的流量轉發到 h1
-curl --user $ONOS_USER:$ONOS_PASS -X POST http://$ONOS_CONTROLLER_IP:8181/onos/v1/flows --header "Content-Type:application/json" --data '{
-  "flows": [{
-    "priority": 40000,
-    "timeout": 0,
-    "isPermanent": true,
-    "deviceId": "of:0000000000000004",
-    "treatment": {
-      "instructions": [
-        {
-          "type": "OUTPUT",
-          "port": "1"
-        }
-      ]
-    },
-    "selector": {
-      "criteria": [
-        {
-          "type": "ETH_TYPE",
-          "ethType": "0x0800"
-        },
-        {
-          "type": "IPV4_SRC",
-          "ip": "10.0.0.2/32"
-        },
-        {
-          "type": "IPV4_DST",
-          "ip": "10.0.0.1/32"
-        }
-      ]
-    }
-  }]
-}'
+# 廣播 ARP 請求的流表規則 (h1)
+curl -X POST -H "Content-Type: application/json" -d '{
+  "priority": 60000,
+  "isPermanent": true,
+  "deviceId": "of:0000000000000001",
+  "treatment": {
+    "instructions": [
+      {
+        "type": "OUTPUT",
+        "port": "FLOOD"
+      }
+    ]
+  },
+  "selector": {
+    "criteria": [
+      {
+        "type": "ETH_TYPE",
+        "ethType": "0x0806"
+      }
+    ]
+  }
+}' http://$CONTROLLER_IP:8181/onos/v1/flows --user $USER:$PASSWORD
 
-# 3. 允許所有的 ICMP 流量（ping）
-curl --user $ONOS_USER:$ONOS_PASS -X POST http://$ONOS_CONTROLLER_IP:8181/onos/v1/flows --header "Content-Type:application/json" --data '{
-  "flows": [{
-    "priority": 30000,
-    "timeout": 0,
-    "isPermanent": true,
-    "deviceId": "of:0000000000000003",
-    "treatment": {
-      "instructions": [
-        {
-          "type": "OUTPUT",
-          "port": "CONTROLLER"
-        }
-      ]
-    },
-    "selector": {
-      "criteria": [
-        {
-          "type": "ETH_TYPE",
-          "ethType": "0x0800"
-        },
-        {
-          "type": "IP_PROTO",
-          "protocol": 1
-        }
-      ]
-    }
-  }]
-}'
+# 廣播 ARP 請求的流表規則 (h2)
+curl -X POST -H "Content-Type: application/json" -d '{
+  "priority": 60000,
+  "isPermanent": true,
+  "deviceId": "of:0000000000000002",
+  "treatment": {
+    "instructions": [
+      {
+        "type": "OUTPUT",
+        "port": "FLOOD"
+      }
+    ]
+  },
+  "selector": {
+    "criteria": [
+      {
+        "type": "ETH_TYPE",
+        "ethType": "0x0806"
+      }
+    ]
+  }
+}' http://$CONTROLLER_IP:8181/onos/v1/flows --user $USER:$PASSWORD
 
-# 4. 設置 TCP 流量優先轉發
-curl --user $ONOS_USER:$ONOS_PASS -X POST http://$ONOS_CONTROLLER_IP:8181/onos/v1/flows --header "Content-Type:application/json" --data '{
-  "flows": [{
-    "priority": 50000,
-    "timeout": 0,
-    "isPermanent": true,
-    "deviceId": "of:0000000000000004",
-    "treatment": {
-      "instructions": [
-        {
-          "type": "OUTPUT",
-          "port": "3"
-        }
-      ]
-    },
-    "selector": {
-      "criteria": [
-        {
-          "type": "ETH_TYPE",
-          "ethType": "0x0800"
-        },
-        {
-          "type": "IP_PROTO",
-          "protocol": 6
-        }
-      ]
-    }
-  }]
-}'
-
-# 5. 阻止來自特定 IP 的流量
-curl --user $ONOS_USER:$ONOS_PASS -X POST http://$ONOS_CONTROLLER_IP:8181/onos/v1/flows --header "Content-Type:application/json" --data '{
-  "flows": [{
-    "priority": 60000,
-    "timeout": 0,
-    "isPermanent": true,
-    "deviceId": "of:0000000000000003",
-    "treatment": {
-      "instructions": []
-    },
-    "selector": {
-      "criteria": [
-        {
-          "type": "ETH_TYPE",
-          "ethType": "0x0800"
-        },
-        {
-          "type": "IPV4_SRC",
-          "ip": "10.0.0.3/32"
-        }
-      ]
-    }
-  }]
-}'
-
-# 6. 允許 UDP 流量轉發
-curl --user $ONOS_USER:$ONOS_PASS -X POST http://$ONOS_CONTROLLER_IP:8181/onos/v1/flows --header "Content-Type:application/json" --data '{
-  "flows": [{
-    "priority": 30000,
-    "timeout": 0,
-    "isPermanent": true,
-    "deviceId": "of:0000000000000004",
-    "treatment": {
-      "instructions": [
-        {
-          "type": "OUTPUT",
-          "port": "CONTROLLER"
-        }
-      ]
-    },
-    "selector": {
-      "criteria": [
-        {
-          "type": "ETH_TYPE",
-          "ethType": "0x0800"
-        },
-        {
-          "type": "IP_PROTO",
-          "protocol": 17
-        }
-      ]
-    }
-  }]
-}'
+echo "All flows have been added successfully."
