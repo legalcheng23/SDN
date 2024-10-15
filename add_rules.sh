c1a9e2422c71536de0c7de03fe5c17092d3d0ddd
Legal <legalcheng23@gmail.com>
2024-10-16 00:15:13 +0800

    Update add_rules.sh

--- add_rules.sh
+++ add_rules.sh
@@ -1,4 +1,6 @@
-curl -X POST -H "Content-Type: application/json" http://172.17.0.5:8181/onos/v1/flows/of:0000000000000001 -d @./flow_h1_to_h2.json --user onos:rocks
-curl -X POST -H "Content-Type: application/json" http://172.17.0.5:8181/onos/v1/flows/of:0000000000000002 -d @./flow_h2_to_h1.json --user onos:rocks
-curl -X POST -H "Content-Type: application/json" http://172.17.0.5:8181/onos/v1/flows/of:0000000000000001 -d @./flow_h1_arp.json --user onos:rocks
-curl -X POST -H "Content-Type: application/json" http://172.17.0.5:8181/onos/v1/flows/of:0000000000000002 -d @./flow_h2_arp.json --user onos:rocks
+curl -X POST -H "content-type:application/json" http://172.17.0.5:8181/onos/v1/flows/of:0000000000000001 -d @./test_1.json --user onos:rocks
+curl -X POST -H "content-type:application/json" http://172.17.0.5:8181/onos/v1/flows/of:0000000000000001 -d @./test_1r.json --user onos:rocks
+curl -X POST -H "content-type:application/json" http://172.17.0.5:8181/onos/v1/flows/of:0000000000000002 -d @./test_2.json --user onos:rocks
+curl -X POST -H "content-type:application/json" http://172.17.0.5:8181/onos/v1/flows/of:0000000000000002 -d @./test_2r.json --user onos:rocks
+curl -X POST -H "content-type:application/json" http://172.17.0.5:8181/onos/v1/flows/of:0000000000000001 -d @./test_1arp.json --user onos:rocks
+curl -X POST -H "content-type:application/json" http://172.17.0.5:8181/onos/v1/flows/of:0000000000000002 -d @./test_2arp.json --user onos:rocks
